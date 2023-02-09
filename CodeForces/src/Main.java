e2733b07eb13e0367f49e70c7eabad7954bc57fa
Decade-qiu <3237550600@qq.com>
2023-02-09 09:25:15 +0800

    2023.2.9

--- CodeForces/src/Main.java
+++ CodeForces/src/Main.java
@@ -1,23 +1,44 @@
-import java.util.*;import java.io.*;
+import java.util.*;
+
+import javax.management.Query;
+
+import java.io.*;
 
 public class Main {
     static String ss, io[];
     static int test, N = 100010, M = 1000000007;
+    static int n, m, a[] = new int[N];
     static void solve() throws Exception{
-        int n = ni(); 
-        long x = ni(), y = ni();
-        long[] dp = new long[2*n+1];
-        Arrays.fill(dp, (long)1e18);
-        dp[0] = 0;
-        for (int i = 0;i <= n;i++){
-            dp[i] = min(dp[i], min(dp[i-1]+x, dp[i+1]+x));
-            dp[i*2] = min(dp[i*2], dp[i]+y);
+        n = ni();
+        for (int i = 1;i <= n;i++) a[i] = ni();
+        int i = 2, j = n-1;
+        while (i <= n && a[i]<=a[i-1]) ++i;
+        while (j >= 1 && a[j]<=a[j+1]) --j;
+        if (i > j){
+            out.println("YES");
+        }else{
+            int f = 0;
+            for (int k = i;k <= j && f == 0;k++){
+                if (a[k] < a[k-1]) f = 1;
+            }
+            if (f == 0){
+                if (a[j]-a[i-1] <= a[j+1]) out.println("YES");
+                else out.println("NO");
+                return;
+            }
+            for (int k = i;k <= j && f == 1;k++){
+                if (a[k] > a[k-1]) f = 0;
+            }
+            if (f == 1){
+                if (a[i]-a[j+1] <= a[i-1]) out.println("YES");
+                else out.println("NO");
+                return;
+            }out.println("NO");
         }
-        out.println(dp[n]);
     }
     public static void main(String[] args) throws Exception {
-        test = 1;
-        // test = ni(in.readLine());
+        // test = 1;
+        test = ni(in.readLine());
         while (test-- > 0){
             solve();
         }out.flush();
