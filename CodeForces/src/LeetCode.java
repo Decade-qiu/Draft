b092f57093a0862e11d31be09a295537751345ad
Decade-qiu <3237550600@qq.com>
2023-02-19 21:49:49 +0800

    2023/2/19

--- CodeForces/src/LeetCode.java
+++ CodeForces/src/LeetCode.java
@@ -25,9 +25,9 @@ public class LeetCode {
         return res;
     }
     public static void main(String[] args) {
-        String[] x = {"[1,1,2,2,2,3]"};
-        System.out.println(new Solution().dieSimulator(
-                2, int1d(x[0])
+        String[] x = {"[3,4,4,5]"};
+        System.out.println(new Solution().squareFreeSubsets(
+                int1d(x[0])
         ));
         System.out.println(Integer.toBinaryString(3));
         System.out.println((3L >> 32 & 1));
@@ -35,28 +35,11 @@ public class LeetCode {
 }
 
 class Solution {
-    public int dieSimulator(int n, int[] rollMax) {
-        int M = (int)(1e9+7), ans = 0;
-        int[][][] dp = new int[n+1][6][16];
-        for (int i = 0;i < 6;i++) dp[0][i][0] = 1;
-        for (int i = 1;i <= n;i++){
-            for (int j = 0;j < 6;j++){
-                for (int k = 2;k <= rollMax[j];k++){
-                    dp[i][j][k] = (dp[i][j][k]+dp[i-1][j][k-1])%M;
-                }
-                for (int t = 0;t < 6;t++){
-                    if (t == j) continue;
-                    for (int k = 0;k <= rollMax[t];k++){
-                        dp[i][j][1] = (dp[i][j][1]+dp[i-1][t][k])%M;
-                    }
-                }
-            }
+    public int squareFreeSubsets(int[] nums) {
+        int[] cnt = new int[31];
+        for (int i : nums) cnt[i]++;
+        for (int i = 1;i < (1<<(31));i++){
+            
         }
-        for (int i = 0;i < 6;i++){
-            for (int j = 1;j <= rollMax[i];j++){
-                ans = (ans + dp[n][i][j])%M;
-            }
-        }
-        return ans;
     }
 }
--- PY/Main.py
+++ PY/Main.py
@@ -1,15 +1,32 @@
 
 def solve():
+    input()
     n, m = list(map(int, input().split()))
-    a = []
-    for i in range(m): a.append(input())
-    for i in range(n-m): x = input()
-    a.sort()
-    for i in a: print(i)
+    g = [list(map(int, input().split())) for i in range(n)]
+    l, r = 1, int(1e9)
+    while l <= r:
+        mid = l+r >> 1
+        if ck(g, mid, n, m): l = mid+1
+        else: r = mid-1
+    print(r)
+
+def ck(g, t, n, m):
+    cnt = [0]*n
+    for i in range(m):
+        hs = set()
+        for j in range(n): 
+            if g[j][i] >= t:
+                hs.add(j)
+                cnt[j] += 1
+        if len(hs) == 0: return False
+    if n < m-1: return True
+    for i in cnt:
+        if i > 1: return True
+    return False
 
     
-
-test = 1 #int(input())
+# test = 1
+test = int(input())
 while test:
     solve()
     test -= 1
