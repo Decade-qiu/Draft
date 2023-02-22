e824b55d6e2298c7096e48db51527d5067acbe93
Decade-qiu <3237550600@qq.com>
2023-02-22 19:31:30 +0800

    2023.2.22

--- CodeForces/src/LeetCode.java
+++ CodeForces/src/LeetCode.java
@@ -25,13 +25,36 @@ public class LeetCode {
         return res;
     }
     public static void main(String[] args) {
-        String[] x = {"[1,0,1]", "[0,0,0]", "[[1,1,1],[2,1,0],[3,0,0]]"};
-        System.out.println(new Solution().handleQuery(
-                int1d(x[0]), int1d(x[1]), int2d(x[2])
+        String[] x = {"[1,0,1]", "[0,0,0]", "[[18,19],[3,12],[17,19],[2,13],[7,10]]"};
+        System.out.println(new Solution().mostBooked(
+                4, int2d(x[2])
         ));
     }
 }
 
 class Solution {
-    
+    public int mostBooked(int n, int[][] meetings) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (x, y)->x[0]==y[0] ? x[1]-y[1] : x[0]-y[0]
+        );
+        Arrays.sort(meetings, (x, y)->x[0]==y[0] ? x[1]-y[1] : x[0]-y[0]);
+        int[] cnt = new int[n];
+        for (int i = 0;i < n;i++) pq.add(new int[]{-1, i});
+        for (int[] c : meetings){
+            List<int[]> tp = new ArrayList<>();
+            while (!pq.isEmpty() && pq.peek()[0] <= c[0]){
+                tp.add(pq.poll());
+            }
+            tp.sort((x, y)->x[1]-y[1]);
+            for (int i = 1;i < tp.size();i++) pq.add(tp.get(i));
+            int[] f = tp.get(0);
+            cnt[f[1]]++;
+            int st = Math.max(f[0], c[0]), last = c[1]-c[0];
+            pq.add(new int[]{st+last, f[1]});
+        }
+        int max = 0;
+        for (int i : cnt) max = Math.max(i, max);
+        for (int i = 0;i < n;i++) if (cnt[i]==max) return i;
+        return 0;
+    }
 }
