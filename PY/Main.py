7f95894f66c360af92c2d6b448ba2639b6d0da94
Decade-qiu <3237550600@qq.com>
2023-02-24 21:22:40 +0800

    2023.2.24

--- PY/Main.py
+++ PY/Main.py
@@ -1,27 +1,18 @@
 
 def solve():
-    s = input()
-    cnt = [0]*26
-    for i in s: cnt[ord(i)-97] += 1
-    odd, even = 0, 0
-    for i in cnt:
-        if i%2==0: even += 1
-        else: odd += 1
-    for i in range(26):
-        if cnt[i]%2 == 1:
-            for j in range(25, i, -1):
-                if cnt[j]%2 == 1:
-                    cnt[i] += 1
-                    cnt[j] -= 1
-                    break
-    ans, ex = "", -1
-    for i in range(26):
-        ans += chr(i+97)*(cnt[i]//2)
-        if cnt[i]%2 == 1: ex = i
-    print(ans+('' if ex==-1 else chr(ex+97))+ans[::-1])
-    
-test = 1
-# test = int(input())
+    n, d, k = list(map(int, input().split()))
+    g = gcd(n, d)
+    loop = n//g
+    k -= 1
+    ans = (k//loop+(k%loop)*d)%n
+    print(int(ans))
+
+def gcd(a, b):
+    if a == 0: return b
+    return gcd(b%a, a)
+
+# test = 1
+test = int(input())
 while test:
     solve()
     test -= 1
