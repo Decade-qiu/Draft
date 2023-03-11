c135aab0325277a872aaacfc3c738a2c049283d4
Decade-qiu <3237550600@qq.com>
2023-03-11 21:17:31 +0800

    2023/3/11

--- CodeForces/src/Main.java
+++ CodeForces/src/Main.java
@@ -8,46 +8,7 @@ public class Main {
     static int[] dx={1,0,0,-1}, dy={0,-1,1,0};
     static int[][] g = new int[101][101], v = new int[101][101];
     static void solve(){
-        n = sc.nextInt(); sc.nextLine();
-        int stx = 0, sty = 0;
-        for(int i = 1;i <= n;i++){
-            String[] c = sc.nextLine().split(" ");
-            for (int j = 1;j <= n;j++){
-                v[i][j] = -1;
-                char cur = c[j-1].charAt(0);
-                if (cur == '+') g[i][j] = 1;
-                else if (cur == '-') g[i][j] = -1;
-                else if (cur == 'B') g[i][j] = 2;
-                else{
-                    g[i][j] = 3;
-                    stx = i; sty = j;
-                }
-            }
-        }
-        Deque<int[]> d = new LinkedList<>();
-        d.offerLast(new int[]{stx, sty});
-        while (!d.isEmpty()){
-            int s = d.size();
-            while (s-- > 0){
-                int[] cur = d.pollFirst();
-                int x = cur[0], y = cur[1];
-                if (v[x][y] != -1) continue;
-                v[x][y] = 1;
-                for (int k = 0;k < 4;k++){
-                    int nx = x+dx[k], ny = y+dy[k];
-                    if (nx<1||ny<1||nx>n||ny>n||v[nx][ny]==1) continue;
-                    if (g[nx][ny] == 2){
-                        System.out.println(ans+1);
-                        return;
-                    }
-                    if (ans==0 || g[x][y]+g[nx][ny]==0) {
-                        d.offerLast(new int[]{nx, ny});
-                    }
-                }
-            }
-            ans++;
-        }
-        System.out.println(-1);
+        
     }
     public static void main(String[] args) throws Exception{
         // try {
